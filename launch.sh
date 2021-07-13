4ae76b47e125b65ff631b1070811de2514f031f2
Flavien Henrion <flavien.henrion@protonmail.com>
2021-07-13 05:04:31 -0400

    authentication OK

--- launch.sh
+++ launch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# DB
+# create postgres_data folder if not exist
 [[ -d postgres_data ]] || mkdir postgres_data
+# launch db (should be stopped)
 docker-compose up -d
+
+# BACK
 cd nestjs_back
+# install module if not exist
+[[ -d node_modules ]] || npm install
+# start back
 npm run start
