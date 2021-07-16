3c9cfd1b082e21ade241cb2d30dc52e22ead5c43
Flavien Henrion <flavien.henrion@protonmail.com>
2021-07-16 08:53:05 -0400

    authentication front test

---
+++ nuxt_front_test/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="page">
+    <LoginPopup v-if="!logged"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LoginPopup from "../components/LoginPopup";
+
+export default Vue.extend({
+  name: "Layout",
+
+  components: {
+    LoginPopup
+  },
+
+  computed: {
+    login() {
+      return false;
+    }
+  }
+})
+</script>
