6617f8ca69c468d6082d61979840f3e53e4ce333
Celia LELOUP <celeloup@e1r10p12.42.fr>
2021-08-18 17:55:41 +0200

    started meteor test

--- pong-test/sketch.js
+++ pong-test/sketch.js
@@ -4,60 +4,71 @@
 
 let leftscore = 0;
 let rightscore = 0;
-let pause = false;
+let pause = true;
 
 function preload() {
-	img = loadImage('assets/ball_white_small.png');
-  }
+	img = loadImage('assets/ball.svg');
+}
 
 function setup() {
-  createCanvas(900, 600);
-//   ding = loadSound('data/ding.mp3');
-  puck = new Puck();
-  left = new Paddle(true);
-  right = new Paddle(false);
+	createCanvas(900, 600);
+	//   ding = loadSound('data/ding.mp3');
+	puck = new Puck();
+	left = new Paddle(true);
+	right = new Paddle(false);
+	meteor = new Meteor();
+	noLoop();
+	frameRate(50);
+	imageMode(CENTER);
+	noSmooth();
+	noStroke();
 }
 
 function draw() {
-  background(0);
+	// background(0);
+	clear();
+	separation();
 
-  puck.checkPaddleRight(right);
-  puck.checkPaddleLeft(left);
+	puck.checkPaddleRight(right);
+	puck.checkPaddleLeft(left);
+	puck.checkMeteor(meteor);
 
-  left.show();
-  right.show();
-  left.update(true, puck.y);
-  right.update(false, 0);
+	left.show();
+	right.show();
+	meteor.show();
+	left.update(true, puck.y);
+	right.update(false, 0);
 
-  puck.update();
-  puck.edges();
-  puck.show();
+	puck.update();
+	puck.edges();
+	puck.show();
 
-  fill(255);
-  textSize(32);
-  text(leftscore, 32, 40);
-  text(rightscore, width - 64, 40);
-  image(img, 100, 100, 12, 12);
+	
+
+	fill(255);
+	textSize(32);
+	text(leftscore, width / 2 - 43, 40);
+	text(rightscore, width / 2 + 20, 40);
 }
 
 function keyReleased() {
-  left.move(0);
-  right.move(0);
+	left.move(0);
+	right.move(0);
 }
 
 function keyPressed() {
-//   console.log(key);
-//   if (key == "ArrowUp") {
-//     left.move(-10);
-//   } else if (key == "ArrowDown") {
-//     left.move(10);
-//   }
+	//   console.log(key);
+	//   if (key == "ArrowUp") {
+	//     left.move(-10);
+	//   } else if (key == "ArrowDown") {
+	//     left.move(10);
+	//   }
 
-  if (key == "ArrowUp") {
-    right.move(-10);
-  } else if (key == "ArrowDown") {
-    right.move(10);
-  }
+	if (key == "ArrowUp") {
+		right.move(-10);
+	} else if (key == "ArrowDown") {
+		right.move(10);
+	}
 }
 
 function stop() {
@@ -66,4 +77,16 @@ function stop() {
 		noLoop();
 	else
 		loop();
+}
+
+function separation() {
+	let wd = 5;
+	let hd = 20;
+	for (let it = 20; it < 600; it += 35)
+	{
+		fill(255);
+		rectMode(CENTER);
+		rect(900 / 2 - wd / 2, it, wd, hd);
+		// console.log(it);
+	}
 }
