310926929730c49fb13d5e3122d0106b1eaebb5d
Celia LELOUP <celeloup@e1r10p12.42.fr>
2021-08-18 17:55:41 +0200

    started meteor test

--- pong-test/puck.js
+++ pong-test/puck.js
@@ -1,84 +1,107 @@
 class Puck {
 	constructor() {
-	  this.x = width / 2;
-	  this.y = height / 2;
-	  this.xspeed = 0;
-	  this.yspeed = 0;
-	  this.r = 12;
-  
-	  this.reset();
+		this.x = width / 2;
+		this.y = height / 2;
+		this.xspeed = 0;
+		this.yspeed = 0;
+		this.r = 12;
+
+		this.reset();
 	}
-  
+
 	checkPaddleLeft(p) {
-	  if (
-		this.y - this.r < p.y + p.h / 2 &&
-		this.y + this.r > p.y - p.h / 2 &&
-		this.x - this.r < p.x + p.w / 2
-	  ) {
-		if (this.x > p.x) {
-		  let diff = this.y - (p.y - p.h / 2);
-		  let rad = radians(45);
-		  let angle = map(diff, 0, p.h, -rad, rad);
-		  this.xspeed = 5 * cos(angle);
-		  this.yspeed = 5 * sin(angle);
-		  this.x = p.x + p.w / 2 + this.r;
+		if (
+			this.y - this.r < p.y + p.h / 2 &&
+			this.y + this.r > p.y - p.h / 2 &&
+			this.x - this.r < p.x + p.w / 2
+		) {
+			if (this.x > p.x) {
+				let diff = this.y - (p.y - p.h / 2);
+				let rad = radians(45);
+				let angle = map(diff, 0, p.h, -rad, rad);
+				this.xspeed = 5 * cos(angle);
+				this.yspeed = 5 * sin(angle);
+				this.x = p.x + p.w / 2 + this.r;
+			}
 		}
-	  }
 	}
 	checkPaddleRight(p) {
-	  if (
-		this.y - this.r < p.y + p.h / 2 &&
-		this.y + this.r > p.y - p.h / 2 &&
-		this.x + this.r > p.x - p.w / 2
-	  ) {
-		if (this.x < p.x) {
-		  let diff = this.y - (p.y - p.h / 2);
-		  let angle = map(diff, 0, p.h, radians(225), radians(135));
-		  this.xspeed = 5 * cos(angle);
-		  this.yspeed = 5 * sin(angle);
-		  this.x = p.x - p.w / 2 - this.r;
+		if (
+			this.y - this.r < p.y + p.h / 2 &&
+			this.y + this.r > p.y - p.h / 2 &&
+			this.x + this.r > p.x - p.w / 2
+		) {
+			if (this.x < p.x) {
+				let diff = this.y - (p.y - p.h / 2);
+				let angle = map(diff, 0, p.h, radians(225), radians(135));
+				this.xspeed = 5 * cos(angle);
+				this.yspeed = 5 * sin(angle);
+				this.x = p.x - p.w / 2 - this.r;
+			}
 		}
-	  }
 	}
-  
+
+	checkMeteor(m) {
+		if (
+			this.y - this.r < m.y + m.h / 2 &&
+			this.y + this.r > m.y - m.h / 2 &&
+			this.x + this.r > m.x - m.w / 2 &&
+			this.x - this.r < m.x + m.w / 2
+		) {
+			console.log('hit meteor');
+			var dx = this.x - m.x;
+			var dy = this.y - (m.y + m.w - 25);
+			var distance = Math.sqrt(dx * dx + dy * dy);
+			if (distance < this.r + m.w) {
+				// let diff = this.y - (m.y - m.h / 2);
+				// let angle = map(diff, 0, m.h, radians(225), radians(135));
+				// this.xspeed = 5 * cos(angle);
+				// this.yspeed = 5 * sin(angle);
+				// this.x = m.x - m.w / 2 - this.r;
+				this.yspeed *= -1;
+			}
+			this.yspeed *= 1.1;
+		}
+	}
+
 	update() {
-	  this.x += this.xspeed;
-	  this.y += this.yspeed;
+		this.x += this.xspeed;
+		this.y += this.yspeed;
 	}
-  
+
 	reset() {
-	  this.x = width / 2;
-	  this.y = height / 2;
-	  let angle = random(-PI / 4, PI / 4);
-	  this.xspeed = 5 * Math.cos(angle);
-	  this.yspeed = 5 * Math.sin(angle);
-  
-	  if (random(1) < 0.5) {
-		this.xspeed *= -1;
-	  }
+		this.x = width / 2;
+		this.y = height / 2;
+		let angle = random(-PI / 4, PI / 4);
+		this.xspeed = 5 * Math.cos(angle);
+		this.yspeed = 5 * Math.sin(angle);
+
+		if (random(1) < 0.5) {
+			this.xspeed *= -1;
+		}
 	}
-  
+
 	edges() {
-	  if (this.y < 0 || this.y > height) {
-		this.yspeed *= -1;
-	  }
-  
-	  if (this.x - this.r > width) {
-		// ding.play();
-		leftscore++;
-		this.reset();
-	  }
-  
-	  if (this.x + this.r < 0) {
-		// ding.play();
-		rightscore++;
-		this.reset();
-	  }
+		if (this.y - this.r < 0 || this.y + this.r > height) {
+			this.yspeed *= -1;
+		}
+
+		if (this.x - this.r > width) {
+			// ding.play();
+			leftscore++;
+			this.reset();
+		}
+
+		if (this.x + this.r < 0) {
+			// ding.play();
+			rightscore++;
+			this.reset();
+		}
 	}
-  
+
 	show() {
-	  fill(255);
-	  ellipse(this.x, this.y, this.r * 2);
+		//   fill(255);
+		//   ellipse(this.x, this.y, this.r * 2);
+		image(img, this.x, this.y, 24, 24);
 	}
-  }
-  
+}
