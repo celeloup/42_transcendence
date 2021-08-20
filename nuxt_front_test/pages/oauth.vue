3288d3f4fd78e60dd8f0116c1ee221ae9e751563
flavien <flavien@42.fr>
2021-08-20 12:18:48 -0400

    new authentification method safari friendly

---
+++ nuxt_front_test/pages/oauth.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  async fetch() {
+    const { $axios, app, store, redirect } = this.$nuxt.context;
+	
+  }
+})
+</script>
