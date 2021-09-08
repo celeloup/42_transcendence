369530c42fabfc9689155660ad8270412b4a2e69
Celia LELOUP <celeloup@e2r7p13.42.fr>
2021-09-08 17:56:20 +0200

    start of front

---
+++ front/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          this is my app
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
