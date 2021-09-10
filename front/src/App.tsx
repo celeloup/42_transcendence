be3f927ad4ba05bdff0dac20e710aa1c0cec009f
Celia LELOUP <celeloup@e2r7p9.42.fr>
2021-09-10 20:39:34 +0200

    started router, navigation buttons, window borders

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+// import React from 'react';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 import './styles/App.scss';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Parameters from './pages/Parameters';
+
+const Logo = () => (
+	<div id="logo">
+		<NavLink to='/'>
+			<p>PonG</p>
+			<p>warS</p>
+		</NavLink>
+	</div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          this is my app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <Router>
+		<div className="App">
+			<header className="App-header">
+				<Logo />
+			</header>
+			<Switch>
+				<Route path='/admin' component={Admin} />
+				<Route path='/login' component={Login} />
+				<Route path='/parameters' component={Parameters} />
+				<Route path='/profile' component={Profile} />
+				<Route path='/' component={Home} />
+          </Switch>
+		</div>
+	  </Router>
   );
 }
 
