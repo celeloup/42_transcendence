1e13ecbb662422737c95aec972104de970944f91
jgonf <jeanne.gonfroy@protonmail.com>
2021-09-13 16:46:16 +0200

    branch check, channel entity done, testeur a changer apres kine

--- nestjs_back/src/channel/channel.entity.ts
+++ nestjs_back/src/channel/channel.entity.ts
@@ -1,17 +1,19 @@
 import { ExecSyncOptionsWithBufferEncoding } from 'child_process';
-import { Column, Entity, ManyToOne, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, ManyToMany, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import User from '../users/user.entity';
+import Message from './message.entity';
 
+
+//add channl dans users
 @Entity()
 class Channel {
   @PrimaryGeneratedColumn()
   public id: number;
 
   @Column()
-  public content: name;
+  public name: string;
 
   @ManyToMany(() => User, (member: User) => member.channels)
-  @JoinTable()
   public members: User[];
 
   @Column({ default: false })
@@ -20,8 +22,20 @@ class Channel {
   @Column({ default: null })
   public password: string;
 
-  
+  @ManyToOne(() => User, (owner: User) => owner.owned)
+  public owner: User;
 
+  @ManyToMany(() => User, (admin: User) => admin.admin)
+  public admin: User[];
+
+  @ManyToMany(() => User, (banned: User) => banned.ban)
+  public banned: User[];
+
+  @ManyToMany(() => User, (muted: User) => muted.mute)
+  public muted: User[];
+
+  @OneToMany(() => Message, (message: Message) => message.channel)
+  public historic: Message[];
 }
 
 export default Channel;
