a86c786c07a7618ed003e3332321c7c80901ce54
pandaketchup <ghermione5@gmail.com>
2021-09-14 00:01:12 +0200

    chat header, messages, scroll bar, start input

--- front/src/ui_components/NavButton.tsx
+++ front/src/ui_components/NavButton.tsx
@@ -4,14 +4,14 @@ import '../styles/UI.scss';
 
 type Props = {
 	name: string;
-	// icon: string;
+	icon: string;
 	link: string;
 }
 
-function NavButton({ name, link }: Props) {
+function NavButton({ name, icon, link }: Props) {
 	return (
 		<NavLink to={ link } className="navButton">
-			{/* <span>I</span> */}
+			<i className={"fas rotate_left " + icon}></i>
 			<span>{ name }</span>
 		</NavLink>
 	)
