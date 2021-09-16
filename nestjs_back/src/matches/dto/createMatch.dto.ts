6a3435f03d3651d978361bbb6e6f7e1dd4717023
amartin-menadier <amenadier@gmail.com>
2021-09-16 12:13:33 +0200

    Cleanup matches dtos

--- nestjs_back/src/matches/dto/createMatch.dto.ts
+++ nestjs_back/src/matches/dto/createMatch.dto.ts
@@ -2,10 +2,6 @@ import { IsEmail, IsString, IsNotEmpty, IsNumber, IsBoolean} from 'class-validat
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export default class CreateMatchDto {
-  @ApiProperty()
-	@IsNumber()
-  public id: number;
-  
   @IsBoolean()
   friendly: boolean;
   
@@ -16,23 +12,4 @@ export default class CreateMatchDto {
   @ApiProperty()
   @IsNumber()
   user2_id: number;
-
-  /* 
-  @IsNumber()
-  @ApiProperty()
-  score_user1: number;
-
-  @IsNumber()
-  @ApiProperty()
-  score_user2: number; 
-  
-  @ApiPropertyOptional()
-  @IsNumber()
-  public winner?: number;
-  
-  //@IsString()
-  //@IsNotEmpty()
-  //@MinLength(7)
-  //password: string;
-  */
 }
