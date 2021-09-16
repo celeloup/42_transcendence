bd4c828dcb5c8700ddd6aff59da5db52731126c2
amartin-menadier <amenadier@gmail.com>
2021-09-16 12:13:33 +0200

    Cleanup matches dtos

--- nestjs_back/src/matches/dto/updateMatch.dto.ts
+++ nestjs_back/src/matches/dto/updateMatch.dto.ts
@@ -2,19 +2,6 @@ import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateMatchDto {
-/* 
-  @ApiProperty()
-	@IsNumber()
-  public id: number;
-  
-  @ApiProperty()
-  @IsNumber()
-  public user1: number;
-
-  @ApiProperty()
-  @IsNumber()
-  public user2: number; */
-  
   @ApiProperty()
   @IsNumber()
   public score_user1: number;
@@ -22,11 +9,6 @@ export class UpdateMatchDto {
   @ApiProperty()
   @IsNumber()
   public score_user2: number;
-/*   
-  @ApiPropertyOptional()
-  @IsNumber()
-  public winner?: number;
- */
 }
    
 export default UpdateMatchDto;
