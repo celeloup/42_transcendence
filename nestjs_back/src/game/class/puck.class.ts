03da37ce342c6174df20144a81dd9f129e115e20
jgonf <jeanne.gonfroy@protonmail.com>
2021-09-25 14:10:49 +0200

    implementation done, besoin de visuel

--- nestjs_back/src/game/class/puck.class.ts
+++ nestjs_back/src/game/class/puck.class.ts
@@ -1,6 +1,5 @@
-import { timeStamp } from 'console';
-import GameService from '../game.service'
 import {height, width, paddle_margin} from '../game.settings'
+import Round from './round.class';
 
 export default class Puck {
     x: number = width / 2;
@@ -10,30 +9,50 @@ export default class Puck {
     y_speed: number = 3;
     indice: number = 1;
 
-    update() {
+    update(param: Round) {
         this.x += this.x_speed;
         this.y += this.y_speed;
-        this.edges();
+        this.edges(param);
     }
 
-	paddle() {
-		if ()
-	}
+    edges(param : Round) {
 
-    edges(param) {
-		if (this.y - this.r < 0 || this.y + this.r > height) {
+        let puck_left = this.x - this.r;
+        let puck_right = this.x + this.r;
+        let puck_bottom = this.y - this.r;
+        let puck_top = this.y + this.r;
+
+        let pladdle1_right = param.paddle_player1.x + param.paddle_player1.w / 2;
+        let pladdle1_bottom = param.paddle_player1.y + param.paddle_player1.h / 2;
+        let pladdle1_top = param.paddle_player1.x - param.paddle_player1.h / 2;
+        
+        let pladdle2_right = param.paddle_player1.x + param.paddle_player1.w / 2;
+        let pladdle2_bottom = param.paddle_player1.y + param.paddle_player1.h / 2;
+        let pladdle2_top = param.paddle_player1.x - param.paddle_player1.h / 2;
+           
+		if (puck_bottom < 0 || puck_top > height) {
          this.y_speed *= -1;
         }
 
-        if (this.x < 0) {
-            this.reset();
-            return 2;
+        if (puck_left < pladdle1_right && puck_top < pladdle1_bottom && puck_bottom > pladdle1_top) {
+            this.x_speed *= -1;
+        }
+        
+        if (puck_right < pladdle2_right && puck_top < pladdle2_bottom && puck_bottom > pladdle2_top) {
+            this.x_speed *= -1;
         }
 
-        if (this.x > width) {
+        if (this.x < 0)
+        {
+            param.id_player2++;
             this.reset();
         }
-
+        
+        if (this.x > width)
+        {
+            param.id_player1++;
+            this.reset();
+        }
     }
 
     reset()
