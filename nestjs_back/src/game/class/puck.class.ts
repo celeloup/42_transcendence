cc8d9c733abd99f7cfe0ad4f217754d60e6508fd
jgonf <jeanne.gonfroy@protonmail.com>
2021-09-27 14:06:36 +0200

    show id when connecton, add timer before start game, add random departure

--- nestjs_back/src/game/class/puck.class.ts
+++ nestjs_back/src/game/class/puck.class.ts
@@ -1,10 +1,10 @@
-import {height, width, paddle_margin} from '../game.settings'
+import { height, width, paddle_margin } from '../game.settings'
 import Round from './round.class';
 
 export default class Puck {
     x: number = width / 2;
     y: number = height / 2;
-	r: number = 12;
+    r: number = 12;
     x_speed: number = 2;
     y_speed: number = 3;
     indice: number = 1;
@@ -15,7 +15,7 @@ export default class Puck {
         this.edges(param);
     }
 
-    edges(param : Round) {
+    edges(param: Round) {
 
         let puck_left = this.x - this.r;
         let puck_right = this.x + this.r;
@@ -25,41 +25,41 @@ export default class Puck {
         let pladdle1_right = param.paddle_player1.x + param.paddle_player1.w / 2;
         let pladdle1_bottom = param.paddle_player1.y + param.paddle_player1.h / 2;
         let pladdle1_top = param.paddle_player1.x - param.paddle_player1.h / 2;
-        
+
         let pladdle2_right = param.paddle_player1.x + param.paddle_player1.w / 2;
         let pladdle2_bottom = param.paddle_player1.y + param.paddle_player1.h / 2;
         let pladdle2_top = param.paddle_player1.x - param.paddle_player1.h / 2;
-           
-		if (puck_bottom < 0 || puck_top > height) {
-         this.y_speed *= -1;
+
+        if (puck_bottom < 0 || puck_top > height) {
+            this.y_speed *= -1;
         }
 
         if (puck_left < pladdle1_right && puck_top < pladdle1_bottom && puck_bottom > pladdle1_top) {
             this.x_speed *= -1;
         }
-        
+
         if (puck_right < pladdle2_right && puck_top < pladdle2_bottom && puck_bottom > pladdle2_top) {
             this.x_speed *= -1;
         }
 
-        if (this.x < 0)
-        {
+        if (this.x < 0) {
             param.id_player2++;
             this.reset();
         }
-        
-        if (this.x > width)
-        {
+
+        if (this.x > width) {
             param.id_player1++;
             this.reset();
         }
     }
 
-    reset()
-    {
-      this.x = width / 2;
-      this.y = height / 2;
-      this.x_speed = 2;
-      this.y_speed = 3;
+    reset() {
+        function getRandomInt(max: number) {
+            return Math.floor(Math.random() * max);
+        }
+        this.x = getRandomInt(width);
+        this.y = getRandomInt(height);
+        this.x_speed *= -1;
+        this.y_speed *= -1;
     }
 }
--- websocket_client/index.js
+++ websocket_client/index.js
@@ -37,7 +37,7 @@ connector.on("connect", () => {
   }, 1000);
  
   setTimeout(() => {
-    connector.emit('paddle_movement', {x: 4, y: 5})
+    connector.emit('paddle_movement', 5)
   }, 2000);
 
   for (const [i, client] of clients.entries()) {
