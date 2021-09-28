1926bd51037852a1078f2f5b02f75fa06d980a95
Celia LELOUP <celeloup@e2r8p7.42.fr>
2021-09-28 17:30:27 +0200

    create chan not done

--- front/src/components/Profile.tsx
+++ front/src/components/Profile.tsx
@@ -9,7 +9,7 @@ function Profile() {
 			this is the Profile page
 		  </p>
 		  { user && <p>id42 = { user.id42 }</p> }
-		  <p><br/>[HERE SEE USERNAME, PROFILE PIC, LIST OF FRIENDS, MATCH HISTORY]</p>
+		  <p><br/>[HERE SEE USERNAME, PROFILE PIC, LIST OF FRIENDS, MATCH HISTORY, ACHIEVEMENT]</p>
 	  </div>
 	);
   }
--- front/src/components/chat/CreateChan.tsx
+++ front/src/components/chat/CreateChan.tsx
@@ -1,44 +1,105 @@
-import { useState, useContext } from 'react';
+import axios from 'axios';
+import { useState, useContext, useEffect } from 'react';
 import { AuthContext, ContextType }  from '../../contexts/AuthContext';
 import '../../styles/CreateChan.scss';
+
+type User = {
+	id: number,
+	name: string
+};
+
 type channelSettings = {
 	type: number,
 	name: string,
 	password: string,
 	members: [],
 	owner_id: number
-}
+};
 
 type CreateChanProps = {
 	type : number,
 	hide : (type: number) => void
-}
+};
 
 function CreateChan({ type, hide } : CreateChanProps) {
 	const { user } = useContext(AuthContext) as ContextType;
 
+	// -------- List users
+	const [users, setUsers] = useState<User[]>([]);
+	useEffect(() => {
+		const getUsers = async () => {
+			try {
+				const res = await axios.get('/users');
+				// console.log(res);
+				setUsers(res.data);
+			}catch (err) {
+				console.log(err);
+			}
+		};
+		getUsers();
+	}, []);
+	
+	var usersList;
+	if (users.length !== 0)
+		usersList = users.map((user:any) => <option key={user.id} value={user.name}/>)
+	else
+		usersList = <p className="no_chan">No user found.</p>
+
+	// ------ Style password
 	const [ typePassword, setTypePassword ] = useState(true);
 
+	// ------- Channel data
 	const [ chanType, setChanType ] = useState<number>(type);
-	const [ chanSettings, setChanSettings ] = useState<channelSettings>(
-		{
-			type: type, 
-			name: "", 
-			password: "", 
-			members: [], 
-			owner_id: user? user.id : 1 // TO CHANGE LATER OR TO REMOVE
-		});
+	const [ chanName, setChanName ] = useState<string>("");
+	const [ chanPassword, setChanPassword ] = useState<string>("");
+	const [ userDM, setUserDM ] = useState<User>();
+	// const [ chanSettings, setChanSettings ] = useState<channelSettings>(
+	// {
+	// 	type: type, 
+	// 	name: "", 
+	// 	password: "", 
+	// 	members: [], 
+	// 	owner_id: user? user.id : 1 // TO CHANGE LATER OR TO REMOVE
+	// });
 	const handleSubmit = (e:any) => {
 		e.preventDefault();
-		setChanSettings({
-			type: type, 
-			name: "", 
-			password: "", 
-			members: [], 
-			owner_id: user? user.id : 1 // TO CHANGE LATER OR TO REMOVE
-		});
-		console.log(chanSettings);
+		let chanRegex = /^[^-\s][a-zA-Z0-9_\s-]+$/;
+		console.log(chanRegex.test(chanName));
+		console.log(chanType);
+		console.log(chanName);
+		console.log(chanPassword);
+		// if (chanType === 1)
+		// {
+		// 	chanSettings.type = 1;
+		// 	chanSettings.password = "";
+		// 	chanSettings.members = [];
+		// }
+		// else if (chanType === 2)
+		// {
+		// 	chanSettings.type = 2;
+		// 	chanSettings.members = [];
+		// }
+		// else if (chanType === 3)
+		// {
+		// 	chanSettings.type = 3;
+		// 	chanSettings.name = "";
+		// }
+		// setChanSettings(prevState => ({
+		// 	...prevState,
+		// 	type: chanType
+		// }));
+		// console.log(chanSettings);
 	}
+
+	// const handleChange = (e:any) => {
+	// 	const { name, value } = e.target;
+	// 	setChanSettings(prevState => ({
+	// 		...prevState,
+	// 		[name]: value
+	// 	}));
+	// 	// console.log(chanSettings);
+	// };
+
 	return (
 		<div id="addChanWrapper">
 			<div id="addChan">
@@ -55,17 +116,47 @@ function CreateChan({ type, hide } : CreateChanProps) {
 					{ chanType === 3 && "/i\\ Start a private conversation with an other member."}
 				</div>
 				<form onSubmit={ handleSubmit }>
-					<label>
-						Name your channel
-						<input autoFocus={true} id="channelName" name="channelName"></input>
-					</label>
-					<label id="passwordLabel">
-						Set a password
-						<input autoComplete="off" type={ typePassword ? "password" : "text" }></input>
-						<span>optional</span>
-						<i className={ typePassword ? "fas fa-eye-slash" : "fas fa-eye" } onClick={ () => setTypePassword(!typePassword)}></i>
-					</label>
-					<input type="submit" value="Create channel"/>
+					{ (chanType === 1 || chanType === 2) && 
+						<label>
+							Name your channel
+							<input 
+								autoFocus={true}
+								autoComplete="off"
+								id="channelName"
+								name="name"
+								value={ chanName }
+								onChange={ (e) => setChanName(e.target.value) }
+							/>
+						</label>
+					}
+					{ chanType === 2 && 
+						<label id="passwordLabel">
+							Set a password
+							<input
+								autoComplete="off"
+								className={ typePassword ? "passwordInput" : ""} 
+								type="text"
+								value={ chanPassword }
+								onChange={ (e) => setChanPassword(e.target.value) }
+							/>
+							<span>optional</span>
+							<i className={ typePassword ? "fas fa-eye-slash" : "fas fa-eye" } onClick={ () => setTypePassword(!typePassword)}></i>
+						</label>
+					}
+					{ chanType === 3 && 
+						<div id="selectUser">
+							ADD USER SELECT HERE
+							{/* <input autoFocus={true} placeholder="Select a user" list="userMp" />
+							<datalist id="userMp">
+								{usersList}
+							</datalist> */}
+							{/* <i className="fas fa-search"></i> */}
+							{/* <div>
+								<ul>{usersList}</ul>
+							</div> */}
+						</div>
+					}
+					<input className={ chanName != "" ? "readyToSubmit" : "" }type="submit" value="Create channel"/>
 				</form>
 			</div>
 		</div>
