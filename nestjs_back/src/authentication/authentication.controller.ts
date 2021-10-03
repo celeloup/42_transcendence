69d4cc8b2bb5cfc6f457e995a17542bc97ab6621
flavien <flavien@42.fr>
2021-10-03 06:20:30 -0400

    back: test login route, no login if ban + no npm update

--- nestjs_back/src/authentication/authentication.controller.ts
+++ nestjs_back/src/authentication/authentication.controller.ts
@@ -19,6 +19,7 @@ import JwtTwoFactorGuard from './guard/jwtTwoFactor.guard';
 import AuthInfos from './interface/authInfos.interface';
 import { ApiResponse, ApiBearerAuth, ApiTags, ApiCookieAuth, ApiOperation, ApiQuery } from '@nestjs/swagger';
 import myUserInfos from './interface/myUserInfos.interface';
+import LoginDto from './dto/LoginDto';
 
 @ApiTags('authentication')
 @Controller('authentication')
@@ -39,6 +40,9 @@ export default class AuthenticationController {
   @ApiResponse({ status: 401, description: '42 Oauth token invalid.'})
   async oauth(@Req() request: RequestWithUser): Promise<AuthInfos> {
     const { user } = request;
+    if (!user || user.site_banned) {
+      throw new UnauthorizedException();
+    }
     const accessJwt = this.authenticationService.getJwtToken(user.id);
     const { accessTokenCookie, accessTokenExpiration } = this.authenticationService.getCookieForJwtToken(accessJwt);
     const refreshJwt = await this.authenticationService.getJwtRefreshToken(user.id);
@@ -66,10 +70,10 @@ export default class AuthenticationController {
   })
   @ApiResponse({ status: 401, description: 'Refresh token invalid.'})
   async refresh(@Req() request: RequestWithUser): Promise<AuthInfos> {
-    if (!request.user) {
+    const { user } = request;
+    if (!user || user.site_banned) {
       throw new UnauthorizedException();
     }
-    const { user } = request;
     const accessJwt = this.authenticationService.getJwtToken(user.id);
     const { accessTokenCookie, accessTokenExpiration } = this.authenticationService.getCookieForJwtToken(accessJwt);
     const refreshJwt = await this.authenticationService.getJwtRefreshToken(user.id);
@@ -101,7 +105,7 @@ export default class AuthenticationController {
   @Get()
   async authenticate(@Req() request: RequestWithUser) {
     const { user } = request;
-    if (!user) {
+    if (!user || user.site_banned) {
       throw new UnauthorizedException();
     }
     return await this.authenticationService.getPrivateInfos(user);
@@ -133,6 +137,31 @@ export default class AuthenticationController {
     };
   }
 
+  // for testing
+  @Post('log-in')
+  @ApiOperation({summary: "login user without 42 oauth for testing purpose"})
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully logged.',
+    type: AuthInfos
+  })
+  async login(@Body() loginData: LoginDto, @Req() req: Request): Promise<AuthInfos> {
+    const fakeUser = await this.authenticationService.findUserFrom42Id(loginData.id42)
+    const accessJwt = this.authenticationService.getJwtToken(fakeUser.id);
+    const { accessTokenCookie, accessTokenExpiration } = this.authenticationService.getCookieForJwtToken(accessJwt);
+    const refreshJwt = await this.authenticationService.getJwtRefreshToken(fakeUser.id);
+    const { refreshTokenCookie, refreshTokenExpiration } = this.authenticationService.getCookieForJwtRefreshToken(refreshJwt);
+    req.res.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
+    return {
+      id: fakeUser.id,
+      name: fakeUser.name,
+      authentication: accessJwt.token,
+      refresh: refreshJwt.token,
+      accessTokenExpiration,
+      refreshTokenExpiration
+    };
+  }
+
   @UseGuards(JwtTwoFactorGuard)
   @Post('log-out')
   @ApiOperation({summary: "delete the token cookies and delete the refresh token from database"})
@@ -142,10 +171,11 @@ export default class AuthenticationController {
   @ApiResponse({ status: 401, description: 'Authenticaation token invalid.'})
   @HttpCode(200)
   async logOut(@Req() request: RequestWithUser) {
-    if (!request.user) {
+    const { user } = request;
+    if (!user || user.site_banned) {
       throw new UnauthorizedException();
     }
-    await this.authenticationService.removeRefreshToken(request.user.id);
+    await this.authenticationService.removeRefreshToken(user.id);
     request.res.setHeader('Set-Cookie', this.authenticationService.getCookieForLogOut());
   }
 }
