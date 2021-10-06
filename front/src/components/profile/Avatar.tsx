63a663387db3d91068c59faa65d21ccfdbc5071f
jgonf <jeanne.gonfroy@protonmail.com>
2021-10-06 17:46:19 +0200

    Ajout de la liste des amis

--- front/src/components/profile/Avatar.tsx
+++ front/src/components/profile/Avatar.tsx
@@ -1,10 +1,17 @@
 import WindowBorder from "../ui_components/WindowBorder";
+import calamityImage from "./logo.jpg";
+import '../../styles/Profile.scss';
 
-function Avatar() {
+type ProfileProps = {
+	alt_img: string,
+    alert_prof: any;
+}
+
+function Avatar ({ alt_img, alert_prof }: ProfileProps) {
     return (
-        <WindowBorder w='500px' h='500px'>
-        <div id="game">
-            <img src={require('./logo.jpg')} />
+        <WindowBorder w='250px' h='250px'>
+        <div id='avatar'>
+            <img src={calamityImage} alt={alt_img} id='picture' onClick={alert_prof} />
         </div>
         </WindowBorder>
     )
