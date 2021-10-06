40655e984b23dd8579d67c18a0edda843fa4cd40
jgonf <jeanne.gonfroy@protonmail.com>
2021-10-06 13:42:54 +0200

    debut afficher image

--- front/src/components/Profile.tsx
+++ front/src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AuthContext, ContextType} from '../contexts/AuthContext';
+import Avatar from './profile/Avatar';
 
 function Profile() {
 	const { user } = React.useContext(AuthContext) as ContextType;
@@ -8,6 +9,7 @@ function Profile() {
 				<p>
 					this is the Profile page
 				</p>
+				<Avatar></Avatar>
 				{ user && <p>id42 = { user.id42 }</p> }
 				<p><br/>[HERE SEE USERNAME, PROFILE PIC, LIST OF FRIENDS, MATCH HISTORY, ACHIEVEMENT]</p>
 			</div>
