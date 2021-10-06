0d6a749620b6f85778620b4e7ecb3c7f1984ed27
jgonf <jeanne.gonfroy@protonmail.com>
2021-10-06 13:42:24 +0200

    fix oubli import module

--- nestjs_back/src/game/game.module.ts
+++ nestjs_back/src/game/game.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import GameGateway from "./game.gateway";
 import AuthenticationModule from "../authentication/authentication.module";
 import GameService from "./game.service";
+import MatchesModule from "src/matches/matches.module";
 
 @Module({
-	imports: [AuthenticationModule],
+	imports: [AuthenticationModule, MatchesModule],
 	providers: [GameGateway, GameService],
 })
 export default class GameModule {}
