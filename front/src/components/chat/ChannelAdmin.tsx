57c4732bb311b1952301088ab8e32caee25a1dc5
celeloup <celia.m.leloup@gmail.com>
2021-10-11 21:40:17 +0200

    [chat admin] added title, infos chan, infos list, hover list, button add member, button leave chan

--- front/src/components/chat/ChannelAdmin.tsx
+++ front/src/components/chat/ChannelAdmin.tsx
@@ -15,20 +15,19 @@ type MemberCardProps = {
 function MemberCard ( { member, channel } : MemberCardProps) {
 	const { name, id } = member;
 
-	const [ displayDrop, setDisplayDrop ] = useState(false);
-
 	var owner = id === channel.owner.id;
-	var banned = channel.banned.some((ban:any) => ban[id] === id) ? "" : "";
-	var muted = channel.muted.some((mute:any) => mute[id] === id) ? "" : "inactive";
+	var admin = channel.admins.some((adm:any) => adm[id] === id) ? "active" : "inactive";
+	var banned = channel.banned.some((ban:any) => ban[id] === id) ? "active" : "inactive";
+	var muted = channel.muted.some((mute:any) => mute[id] === id) ? "active" : "inactive";
 
 	return (
 		<div className="member_card">
+			{ name }
 			<div className="member_status">
-				{ name }
 				{ owner && <i className="fas fa-crown"></i> }
-				{ !owner && <i className="fas fa-shield-alt"></i> }
-				{ !owner && <i className={ `fas fa-user-slash action ${ banned }` }></i> }
-				{ !owner && <i className={ `fas fa-comment-slash action ${ muted }` }></i> }
+				{ !owner && <i className={ `fas fa-shield-alt action good ${ admin }` }></i> }
+				{ !owner && <i className={ `fas fa-user-slash action bad ${ banned }` }></i> }
+				{ !owner && <i className={ `fas fa-comment-slash action bad ${ muted }` }></i> }
 			</div>
 		</div>
 	)
@@ -61,10 +60,31 @@ export function ChannelAdmin () {
 
 	return (
 		<div id="channel_admin">
-			{/* hello this is the admin chan */}
 			<i className="fas fa-times close_icon" onClick={ toggleDisplayAdmin }></i>
+			<div id="name_description">
+				<i className="fas fa-user-friends"></i> { channel?.name }
+				<p>This channel is public. Everyone can join it.</p>
+			</div>
+			<div id="info_members">
+				<i className="fas fa-info-circle"></i>
+				<div>
+					<span><i className="fas fa-crown"></i>Owner</span>
+					<span><i className="fas fa-shield-alt"></i>Admin</span>
+					<span><i className="fas fa-comment-slash"></i>Muted</span>
+					<span><i className="fas fa-user-slash"></i>Banned</span>
+				</div>
+			</div>
 			<div id="member_list">
 				{ members }
+			</div>
+			<div id="add_member_button">
+				<i className="fas fa-user-plus"></i>
+				Add a member
+			</div>
+			<div id="leave_button">
+				<i className="fas fa-door-closed"></i>
+				<i className="fas fa-door-open"></i>
+				Leave the channel
 			</div>
 		</div>
 	)
