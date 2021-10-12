af7838970f52de83535d5cf06cdb921d9b8bc2ab
Celia LELOUP <celeloup@e1r8p11.42.fr>
2021-10-12 16:21:31 +0200

    first test game

--- front/src/components/game/Game.tsx
+++ front/src/components/game/Game.tsx
@@ -1,9 +1,11 @@
 
-// import { io } from "socket.io-client";
-// import { useContext, useState, useEffect } from 'react';
+import { io } from "socket.io-client";
+import { EffectCallback, useState, useEffect } from 'react';
 import WindowBorder from '../ui_components/WindowBorder';
-// import Sketch from "react-p5";
-// import p5Types from "p5";
+import Sketch from "react-p5";
+import p5Types from "p5";
+import P5 from "p5";
+import axios from "axios";
 
 // type GameSettings = {
 // 	friendly: boolean, 
@@ -19,63 +21,111 @@ import WindowBorder from '../ui_components/WindowBorder';
 // 	id_player1: number
 // }
 
+type Paddle = {
+	is_left: boolean,
+	x: number,
+	y: number,
+	w: number,
+	h: number,
+	indice: number
+}
+
 function Game() {
+	
+	const [ socket, setSocket ] = useState<any>(null);
+	const [ puck, setPuck ] = useState<{x: number, y: number}>({x:100, y:100});
+	const [ paddleRight, setPaddleRight ] = useState<Paddle>({ is_left: false, x: 0, y: 0, w: 20, h: 80, indice:0 });
+	const [ paddleLeft, setPaddleLeft ] = useState<Paddle>({ is_left: true, x: 0, y: 0, w: 20, h: 80, indice:0 });
+	const [ score, setScore ] = useState<number[]>([0, 0]);
+	const [ match, setMatch ] = useState<any>(null);
 
-	// const [socket, setSocket] = useState<any>(null);
-	// const [ puck, setPuck ] = useState<{x: number, y: number}>({x:100, y:100});
+	useEffect(() : ReturnType<EffectCallback> => {
+		const newSocket:any = io(`http://localhost:8080/game`, { transports: ["websocket"] });
+		setSocket(newSocket);
+		return () => newSocket.close();
+	}, [setSocket]);
 
-	// useEffect(() => {
-	// 	setSocket(io("http://localhost:8080/game", { transports: ["websocket"] }));
-	// }, [])
+	useEffect(() => {
+		socket?.on('new_frame', (data:any) => {
+			// console.log("new frame :", data);
+			setPuck(data.puck);
+			setPaddleLeft(data.paddle_player1);
+			setPaddleRight(data.paddle_player2);
+			setScore([data.score_player1, data.score_player2]);
+		})
 
-	// useEffect(() => {
-	// 	// console.log("in useEffect");
-	// 	socket?.on('new_frame', (data:any) => {
-	// 		// console.log("on new frame");
-	// 		// console.log(data);
-	// 		setPuck(data.puck);
-	// 	})
-	// })
+		socket?.on('finish_game', (data:any) => {
+			alert("game finished !");
+		})
+	}, [socket])
 
-	// const launch_game = () => {
-	// 	// socket.emit('launch_game', { user1_id: 1, user2_id: 2 });
-	// }
+	const launch_game = () => {
+		if (match === null)
+			console.log("CREATE MATCH BEFORE LAUNCH");
+		else
+			socket.emit('launch_game', match);
+	}
+
+	const create_game = () => {
+		axios.post('/matches', {
+			"friendly": true,
+			"user1_id": 1,
+			"user2_id": 2
+		  })
+		  .then( res => { 
+			  console.log("create match success !", res);
+			  	socket.emit('join_game', res.data.id);
+				setMatch(res.data)
+			} )
+		  .catch ( err => { console.log("create match fail :("); } )
+	}
 
 	// ------------- CANVAS
 
-	// function separation(p5: p5Types) {
-	// 	let wd = 5;
-	// 	let hd = 20;
-	// 	for (let it = 20; it < 630; it += 35)
-	// 	{
-	// 		p5.fill(255);
-	// 		p5.rectMode(p5.CENTER);
-	// 		p5.rect(782 / 2 - wd / 2, it, wd, hd);
-	// 		// console.log(it);
-	// 	}
-	// }
+	function separation(p5: p5Types) {
+		let wd = 5;
+		let hd = 20;
+		for (let it = 20; it < 630; it += 35)
+		{
+			p5.fill(255);
+			p5.rectMode(p5.CENTER);
+			p5.rect(782 / 2 - wd / 2, it, wd, hd);
+		}
+	}
 
-	// const setup = (p5: p5Types, canvasParentRef: Element) => {
-	// 	p5.createCanvas(782, 630).parent(canvasParentRef);
-	// };
+	const setup = (p5: p5Types, canvasParentRef: Element) => {
+		p5.createCanvas(782, 630).parent(canvasParentRef);
+	};
 
-	// const draw = (p5: p5Types) => {
-	// 	p5.background(0);
-	// 	separation(p5);
-	// 	// p5.ellipse(puck.x, puck.y, 20, 20);
-	// 	// puck.x++;
-	// };
-	
+	const draw = (p5: p5Types) => {
+		p5.background(0);
+		separation(p5);
+		p5.ellipse(puck.x, puck.y, 20, 20);
+		p5.rect(paddleLeft.x, paddleLeft.y, paddleLeft.w, paddleLeft.h);
+		p5.rect(paddleRight.x, paddleRight.y, paddleRight.w, paddleRight.h);
+	};
+
+	const keyPressed = (p5: p5Types) => {
+		if (p5.key === "ArrowUp") {
+			console.log("up");
+			socket.emit('paddle_movement', match.id, "up")
+		} else if (p5.key === "ArrowDown") {
+			console.log("down");
+			socket.emit('paddle_movement', match.id, "down")
+		}
+	};
 
 	return (
 		<WindowBorder w='816px' h='670px'>
 			
 		<div id="game">
 			<div className="window_header">
-				{/*MACHIN VS TRUC <button onClick={launch_game}>LAUNCH GAME</button>
+				<button onClick={ create_game }>CREATE GAME</button>
+				{ score[0] } MACHIN VS TRUC { score[1] }
+				<button onClick={ launch_game }>LAUNCH GAME</button>
 			</div>
 			<div id="game_window">
-				{/* <Sketch setup={setup} draw={draw} /> */}
+				<Sketch setup={ setup } draw={ draw } keyPressed={ keyPressed } />
 			</div>
 		</div>
 	</WindowBorder>
