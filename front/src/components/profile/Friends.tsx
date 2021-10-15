b32f31bb56df300aa80ead4961eff5e0f0278455
jgonf <jeanne.gonfroy@protonmail.com>
2021-10-15 10:23:37 +0200

    just rm a comment

--- front/src/components/profile/Friends.tsx
+++ front/src/components/profile/Friends.tsx
@@ -15,8 +15,6 @@ function Friend ({username}: friendProp) {
 	const [online, setOnline] = useState(false);
     return (
         <div className ='friends_info'>
-    		{/* donner une classe avec width de 100 pour cent qui revient a la ligne */}
-    	    {/* <img id='avatar' src={calamityImage} alt='cowgirl'/> */}
             <div id='avatar' className='generated'> { username.charAt(0)}</div>
 			<p>{username}</p>
 	    	<div className={`dot_status ${online ? 'online': 'offline'}`} ></div>
