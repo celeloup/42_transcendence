b5d45f82b612795dbd1be0a26fddb0ae42fb3bd2
celeloup <celia.m.leloup@gmail.com>
2021-10-17 18:19:04 +0200

    Ghost auth good, fixed start position of paddles and puck, fixed game window size, fixed puck size, minor style fixes (gap to margin for bowser compatibility)

--- front/src/components/Login.tsx
+++ front/src/components/Login.tsx
@@ -11,14 +11,14 @@ function Login() {
 	const { login } = useContext(AuthContext) as ContextType;
 
 	const [ ghostLogin, setGhostLogin ] = useState<number>(0);
-	const ghost_register = (id42:number) => {
+	const ghost_authenticate = (id42:number) => {
 		setGhostLogin(id42);
 	};
 
 	useEffect(() => {
 		if (ghostLogin !== 0)
 		{
-			axios.post(`/authentication/register`, { "id42": 30, "email": "ghosty@mail.com", "name":"ghosty" })
+			axios.post(`/authentication/log-in`, { "id42": ghostLogin })
 			.then (response => {
 				// console.log(response);
 				login(response.data);
@@ -27,23 +27,6 @@ function Login() {
 			})
 			.catch(error => {
 				console.log("Error catch :", error);
-				// if (!error.data)
-				// 	alert("Looks like the back is down !!\n\nERR_EMPTY_RESPONSE");
-				// if (error.data.statusCode === 404)
-				// {
-				// 	console.log(error);
-					// axios.post(`/authentication/register`, { "id42": 1, email:"ghosty@mail.com", name: "ghosty"})
-					// .then (response => {
-					// 	console.log(response);
-					// 	login(response.data);
-					// 	setRedir(true);
-					// 	// setGhostLogin(0);
-					// })
-					// .catch(error => {
-					// 	console.log("Error catch :", error);
-					// 	// setGhostLogin(0);
-					// })
-				// }
 				setGhostLogin(0);
 			})
 		}
@@ -58,10 +41,10 @@ function Login() {
 					<Logo42 /> LOGIN
 				</a>
 				<div id="ghosts">
-					<div className="ghost_button" onClick={ () => { ghost_register(1); } }><i className="fas fa-ghost"></i>GHOSTY</div>
-					{/* <div className="ghost_button" onClick={ () => { ghost_register(2); } }><i className="fas fa-ghost"></i>ADMINISTRAGHOST</div>
-					<div className="ghost_button" onClick={ () => { ghost_register(3); } }><i className="fas fa-ghost"></i>MODOGHOST</div>
-					<div className="ghost_button" onClick={ () => { ghost_register(4); } }><i className="fas fa-ghost"></i>CASPER</div> */}
+					<div className="ghost_button" onClick={ () => { ghost_authenticate(1); } }><i className="fas fa-ghost"></i>GHOSTY</div>
+					<div className="ghost_button" onClick={ () => { ghost_authenticate(2); } }><i className="fas fa-ghost"></i>ADMINISTRAGHOST</div>
+					<div className="ghost_button" onClick={ () => { ghost_authenticate(4); } }><i className="fas fa-ghost"></i>MODOGHOST</div>
+					<div className="ghost_button" onClick={ () => { ghost_authenticate(3); } }><i className="fas fa-ghost"></i>CASPER</div>
 				
 				</div>
 			</div>
