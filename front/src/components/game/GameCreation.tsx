fe132e82ae19764dcd58b0a8a483122ab4586b58
Celia LELOUP <celeloup@e3r8p7.42.fr>
2021-10-19 18:40:59 +0200

    Game settings almost done

--- front/src/components/game/GameCreation.tsx
+++ front/src/components/game/GameCreation.tsx
@@ -1,15 +1,16 @@
 import { useContext, useState } from 'react';
 import { GameContext, ContextType } from '../../contexts/GameContext';
-import UpArrow from '../../assets/img/up_arrow.svg';
-import DownArrow from '../../assets/img/down_arrow.svg';
+import { Arrow } from '../ui_components/Arrow';
+import { ToggleButton } from '../ui_components/ToggleButton';
+import { Speedometer } from '../ui_components/Speedometer';
 
 function GameCreation() {
 
 	var { setToDisplay } = useContext(GameContext) as ContextType;
 	const [ goal, setGoal ] = useState(10); // entre 1 et 20
 	const [ speed, setSpeed ] = useState(1); // 0 slow, 1 normal, 2 fast
-	const [ map, setMap ] = useState(1); // 1 space, 2 mario
-	const [ boost, setBoost ] = useState(0); // 0 off, 1 on
+	const [ map, setMap ] = useState(3); // 1 space, 2 mario (street fighter ? )
+	const [ boost, setBoost ] = useState(false);
 
 	// const create_game = () => {
 	// 	axios.post('/matches', {
@@ -30,19 +31,48 @@ function GameCreation() {
 	// }
 
 
-	function updateGoal(indice:number){
-		if (indice == 1 && goal < 20)
+	function updateGoal(direction: string){
+		if (direction === "up" && goal < 20)
 			setGoal(goal + 1);
-		if (indice == -1 && goal > 1)
+		else if (direction === "down" && goal > 1)
 			setGoal(goal - 1);
 	}
 	
-	function updateSpeed(indice:number){
-		if (indice == 1 && speed < 3)
-			setGoal(goal + 1);
-		if (indice == -1 && goal > 1)
-			setGoal(goal - 1);
+	function updateSpeed(){
+		if (speed === 2)
+			setSpeed(0);
+		else
+			setSpeed(speed + 1);
 	}
+
+	function updateBoost(direction: string) {
+		if (direction === "up" && boost)
+			setBoost(false);
+		else if (direction === "down" && !boost)
+			setBoost(true);
+	}
+
+	function updateMap(direction: string) {
+		if (map === 3 && direction === "down")
+			setMap(1);
+		else if (direction === "down")
+			setMap(map + 1);
+		else if (map === 1 && direction === "up")
+			setMap(3);
+		else
+			setMap(map - 1);
+		// if (direction === "up" && map < 3)
+		// 	setMap(map + 1);
+		// else if (direction === "down" && map > 1)
+		// 	setMap(map - 1);
+	}
+
+	// function updateMap(direction: string) {
+	// 	if (direction === "up" && boost !== 1)
+			
+	// }
+
+	// console.log("goal, speed, boost, map", goal, speed, boost, map);
 
 	// has a user2 as optional param in case challenge from chat
 	return (
@@ -54,27 +84,30 @@ function GameCreation() {
 			<div id="game_settings_button">
 				<div id="goal">
 					<div>GOAL</div>
-					<img className="settings_arrows" src={ UpArrow } onClick={() => updateGoal(1)} />
+					<Arrow direction="up" state={ goal < 20 ? "active" : "disabled" } click={ updateGoal }/>
 					<div className="settings_value">{ goal }</div>
-					<img className="settings_arrows" src={ DownArrow } onClick={() => updateGoal(-1)}/>
+					<Arrow direction="down" state={ goal > 1 ? "active" : "disabled" } click={ updateGoal }/>
 				</div>
 				<div id="speed">
 					<div>SPEED</div>
-					<img className="settings_arrows" src={ UpArrow } />
-					<div className="settings_value">{ speed === 1 ? "normal" : (speed === 0 ? "slow" : "fast") }</div>
-					<img className="settings_arrows" src={ DownArrow } />
+					<div onClick={ updateSpeed } className={ speed === 0 ? "settings_value slow_speed" : "settings_value" }>
+						<Speedometer speed={ speed} />
+						<p className={speed === 2 ? "fast_speed" : "" }>{ speed === 1 ? "normal" : (speed === 0 ? "slow" : "fast") }</p>
+					</div>
 				</div>
 				<div id="boost">
 					<div>BOOST</div>
-					<img className="settings_arrows" src={ UpArrow } />
-					<div className="settings_value">{ boost === 1 ? "on" : "off" }</div>
-					<img className="settings_arrows" src={ DownArrow } />
+					<div id="toggle">
+						<ToggleButton direction={ boost ? "down" : "up" }/>
+						<p id="off" onClick={ () => updateBoost("up") } className={ boost ? "disabled" : "active" }>off</p>
+						<p id="on" onClick={ () => updateBoost("down") } className={ boost ? "active" : "disabled" }>on</p>
+					</div>
 				</div>
 				<div id="map">
 					<div>MAP</div>
-					<img className="settings_arrows" src={ UpArrow } />
-					<div className="settings_value">{ map === 1 ? "space" : "mario" }</div>
-					<img className="settings_arrows" src={ DownArrow } />
+					<Arrow direction="up" state={ "active" } click={ updateMap }/>
+					<div className={ map === 1 ? "space settings_value" : (map === 2 ? "mario settings_value" : "street settings_value") }><p>{ map === 1 ? "space" : "mario" }</p></div>
+					<Arrow direction="down" state={ "active"  } click={ updateMap }/>
 				</div>
 			</div>
 			{/* <div>Send invitation</div> */}
