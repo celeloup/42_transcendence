34a57c5ac113444676c2bacefb3b9ac2c4b4824a
jgonf <jeanne.gonfroy@protonmail.com>
2021-10-21 18:08:57 +0200

    boost done

--- nestjs_back/src/game/class/boost.class.ts
+++ nestjs_back/src/game/class/boost.class.ts
@@ -2,6 +2,12 @@ export default class Boost {
     x: number;
     y: number;
     type: number;
-    desactivation_type: number;
-    launch: boolean;
+    ending_condition: {"score1": number, "score2": number};
+    launch: boolean = false;
+
+    constructor( x: number, y: number, type: number) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
 }
