043ed34480370953f46b8af7dd2c1818e59b76c2
César Claude <cesarclaudepro@gmail.com>
2021-10-25 14:49:11 +0200

    minor merge fixes

--- front/src/components/Profile.tsx
+++ front/src/components/Profile.tsx
@@ -38,22 +38,24 @@ function Profile (props : any) {
 	const [nbPoints, setNbPoints] = React.useState<number>(0);
 	const [rank, setRank] = React.useState<number>(0);
 	const [friends, setFriends] = React.useState<Friend[]>([]);
+	
+	const userId = +props.match.params.id;
 
 	useEffect(() => {
-		axios.get("/users/infos/" + props.match.params.id)
+		axios.get("/users/infos/" + userId)
 		.then(response => { setUsername(response.data.name);
 							setHasAvatar(response.data.avatar !== null)
 						})
 		.catch(error => { console.log(error.response); });
 
-		axios.get("/users/matches/" + props.match.params.id)
+		axios.get("/users/matches/" + userId)
 		.then(response => { setMatches(response.data);
 							setNbMatches(response.data.length);
 							setNbVictories(response.data.filter(function(match : Match) {
-								return (match.winner === props.match.params.id);
+								return (match.winner === userId);
 							}).length);
 							setNbPoints(response.data.map(function(match : Match) {
-								if (match.user1_id === props.match.params.id)
+								if (match.user1_id === userId)
 									return (match.score_user1);
 								else
 									return (match.score_user2);
@@ -61,24 +63,24 @@ function Profile (props : any) {
 		})
 		.catch(error => { console.log(error.response); })
 
-		axios.get("/users/friends/" + props.match.params.id)
+		axios.get("/users/friends/" + userId)
 		.then(response => { setFriends(response.data); })
 		.catch(error => { console.log(error.response); });
 
 		axios.get("/users/ranked")
-		.then(response => { setRank(response.data.map((e : any) => e.id).indexOf(props.match.params.id)); })
+		.then(response => { setRank(response.data.map((e : any) => e.id).indexOf(userId)); })
 		.catch(error => { console.log(error.response); })
-	}, [props.match.params.id]);
+	}, [userId]);
 
 	return (
 			<div className="profile">
 				<div id="column_left">
-					<UserCard user_name={username} user_id={props.match.params.id} has_avatar={hasAvatar} rank={rank}
+					<UserCard user_name={username} user_id={userId} has_avatar={hasAvatar} rank={rank}
 						nb_matches={nbMatches} nb_victories={nbVictories} nb_points={nbPoints}/>
 					<Friends friends={friends}/>
 				</div>
 				<div id="column_right">
-					<MatchHistory matches={matches} my_id={props.match.params.id}/>
+					<MatchHistory matches={matches} my_id={userId}/>
 					<Achievements />
 				</div>
 			</div>
