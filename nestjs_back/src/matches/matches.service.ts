188c76f396ecc0edb00840c3075cd8fd5f62cd03
amartin-menadier <amenadier@gmail.com>
2021-11-11 10:08:38 +0100

    users in match fixed

--- nestjs_back/src/matches/matches.service.ts
+++ nestjs_back/src/matches/matches.service.ts
@@ -2,11 +2,11 @@ import { HttpException, HttpStatus, Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import Match from './match.entity';
-import * as bcrypt from 'bcrypt';
 import CreateMatchDto from './dto/createMatch.dto';
 import UsersService from 'src/users/users.service';
 import User from 'src/users/user.entity';
 import AchievementsService from 'src/achievements/achievements.service';
+import UpdateMatchDto from './dto/updateMatch.dto';
 
 @Injectable()
 export default class MatchesService {
@@ -32,12 +32,19 @@ export default class MatchesService {
     throw new HttpException('No matches found', HttpStatus.NOT_FOUND);
   }
 
-  async getMatchById(id: number) {
+  async getMatchByIdNoThrow(id: number) {
     const match = await this.matchesRepository.findOne(id, {
       relations: ['users'], order: {
         createdDate: "DESC"
       }
     });
+    if (match) {
+      return match;
+    }
+  }
+
+  async getMatchById(id: number) {
+    const match = await this.getMatchByIdNoThrow(id)
     if (match) {
       return match;
     }
@@ -127,12 +134,38 @@ export default class MatchesService {
     return match;
   }
 
+  // async updateMatchTest(id: number, updatedMatch: UpdateMatchDto) {
+  //   //await this.getMatchById(id);
+  //   if (updatedMatch.score_user1 === updatedMatch.goal || updatedMatch.score_user2 === updatedMatch.goal)
+  //     return await this.weHaveAWinner(updatedMatch);
+  //   return await this.matchesRepository.save(updatedMatch);
+  // }
+
+  async updateMatchUsers(match: &Match, user1_id: number, user2_id: number){
+    if (match.user1_id !== user1_id){
+      let user1 = await this.usersService.getByIdNoThrow(user1_id);
+      match.users[0] = user1;
+      match.user1_id = user1_id;
+    }    
+    if (match.user2_id !== user2_id){
+      let user2 = await this.usersService.getByIdNoThrow(user2_id);
+      match.users[1] = user2;
+      match.user2_id = user2_id;
+    }
+    await this.matchesRepository.save(match);
+    return match;
+  } 
+
   //called from a websocket
   async updateMatch(id: number, updatedMatch: & Match) {
-    //await this.getMatchById(id);
+    let originMatch = await this.getMatchByIdNoThrow(id);
+    if (originMatch.user1_id !== updatedMatch.user1_id || originMatch.user2_id !== updatedMatch.user2_id)
+      originMatch = await this.updateMatchUsers(originMatch, updatedMatch.user1_id, updatedMatch.user2_id)
+    originMatch.score_user1 = updatedMatch.score_user1;
+    originMatch.score_user2 = updatedMatch.score_user2;    
     if (updatedMatch.score_user1 === updatedMatch.goal || updatedMatch.score_user2 === updatedMatch.goal)
-      return await this.weHaveAWinner(updatedMatch);
-    return await this.matchesRepository.save(updatedMatch);
+      return await this.weHaveAWinner(originMatch);
+    return await this.matchesRepository.save(originMatch);
   }
 
   //called from a websocket
